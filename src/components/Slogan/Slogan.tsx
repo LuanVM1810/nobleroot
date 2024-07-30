6f0c563b052657be5a26bcfd6264b2b60bd4f655
LuanVM1810 <Vomongluan@gmail.com>
2024-07-30 16:36:17 +0700

    fix responsive

--- src/components/Slogan/Slogan.tsx
+++ src/components/Slogan/Slogan.tsx
@@ -2,8 +2,8 @@ import Card from "../../assets/isocard.svg";
 
 const Slogan = () => {
   return (
-    <section className="text-36 my-14 font-extrabold md:my-28 md:grid md:grid-cols-4 md:items-center md:text-48">
-      <div className="text-center md:col-span-2 md:col-start-2">
+    <section className="text-36 my-14 font-extrabold md:my-28 md:grid md:grid-cols-3 md:items-center md:text-48">
+      <div className="text-center md:col-span-1 md:col-start-2">
         <p>
           Earn <span className="text-purple-300">More</span>
         </p>
