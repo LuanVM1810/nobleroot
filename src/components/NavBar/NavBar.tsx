586a5f23a8147ba169043985de32107782d43190
LuanVM1810 <Vomongluan@gmail.com>
2024-07-30 15:15:35 +0700

    add menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
 import { HiOutlineMenu } from "react-icons/hi";
 import NobleRootLogo from "../../assets/NobleRoot (9).png";
-
+import { useState } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 const NavBar = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
+  const openMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
   return (
     <nav className="flex items-center">
       <div className="flex items-center">
@@ -9,9 +15,32 @@ const NavBar = () => {
         <FaBitcoin size={"24px"} color="#e9c463" className="cursor-pointer" /> */}
         <img src={NobleRootLogo} alt="" width={150} height={80} />
       </div>
-      <ul className="ml-auto text-16 font-semibold ">
-        <HiOutlineMenu size={"24px"} />
-      </ul>
+      <div className="ml-auto text-16 font-semibold relative">
+        {isOpenMenu ? (
+          <IoCloseOutline
+            onClick={openMenu}
+            className="cursor-pointer"
+            size={"24px"}
+          />
+        ) : (
+          <HiOutlineMenu
+            onClick={openMenu}
+            className="cursor-pointer"
+            size={"24px"}
+          />
+        )}
+
+        {isOpenMenu && (
+          <div className="bg-white z-1 text-black py-2 px-4 absolute right-4 rounded-lg">
+            <ul>
+              <li>Courses</li>
+              <li>Sign up</li>
+              <li>Sign in</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
